aaa9be5af952d2ee8d8300e99cd5a4d17940a5e8
Romain Guy <romainguy@curious-creature.com>
2014-11-10 16:27:44 +0100

    Remove unnecessary import

--- app/src/main/java/com/example/android/io2014/MainActivity.java
+++ app/src/main/java/com/example/android/io2014/MainActivity.java
@@ -25,7 +25,6 @@ import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
